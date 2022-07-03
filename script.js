5544b0ad45a613421a9714e2d4ce1cf9b70d39f3
Jonnevie <jonnevie@gmail.com>
2022-07-04 09:37:00 +1000

    everything working except edit and delete task and persisting tasks localstorage API.

--- script.js
+++ script.js
@@ -1,129 +1,124 @@
-window.addEventListener('load', () => {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-    })
-})
-
-
-
-// the Date Functionality
-
-let today = new Date();
-let dd = String(today.getDate()).padStart(2,'0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let yyy = today.getFullYear();
-let hh= String(today.getHours()).padStart(2,'0');
-let min = String(today.getMinutes()).padStart(2,'0');
-
-
-let time = `${hh}:${min}`;
-today = `${dd}-${mm}-${yyy}`;
-let dateSpan = document.getElementById('dateSpan');
-
-
-dateSpan.innerHTML= `<strong>Date:</strong> ${today} |
- <strong>Time:</strong> ${time}`;
-
- let dateSpanMobile = document.getElementById('dateSpanMobile');
- dateSpanMobile.innerHTML= `<strong>Date:</strong> ${today} |
- <strong>Time:</strong> ${time}`;
-
-
-
-//All the lets in the house
-let taskName = document.getElementById('taskName');
-let assignedTo = document.getElementById('assignedTo');
-let form = document.getElementById('form');
-let setStatus = document.getElementById('setStatus');
-let errorElement = document.getElementById('errorMsg');
-let description = document.getElementById('description');
-var modal = document.getElementById("form");
-var btn = document.getElementById("myBtn");
-var span = document.getElementById("closebtn");
-let dueDate = document.getElementById('dueDate');
-let formDelete = document.getElementById('formDelete');
-let closebtnedit = document.getElementById('closebtnedit');
-let card1 = document.getElementsByClassName("card1");
-
-
-
-let modalOverlay = document.getElementById('modalOverlay');
-let mobileAddTaskBtn = document.getElementById('addTaskBtnMobile');
-
-
-
-
-//Click events
-
-  for (let i = 0; i < card1.length; i++) {
-    card1.item(i).addEventListener('click', function(){
-     formDelete.style.display = 'block'
-     modalOverlay.style.opacity = "0.3";
-     modalOverlay.style.backgroundColor = "gray";
+window.addEventListener("load", () => {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
     });
- }
-btn.onclick = function() {
-  modal.style.display = "block";
-  modalOverlay.style.opacity = "0.3";
-  modalOverlay.style.backgroundColor = "gray";
+  });
   
-}
-span.onclick = function() {
-  modal.style.display = "none";
-  modalOverlay.style.opacity = "1";
-  modalOverlay.style.backgroundColor = "transparent";
-}
-closebtnedit.onclick = function() {
+  // the Date Functionality
+  
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0");
+  let yyy = today.getFullYear();
+  let hh = String(today.getHours()).padStart(2, "0");
+  let min = String(today.getMinutes()).padStart(2, "0");
+  
+  let time = `${hh}:${min}`;
+  today = `${dd}-${mm}-${yyy}`;
+  let dateSpan = document.getElementById("dateSpan");
+  
+  dateSpan.innerHTML = `<strong>Date:</strong> ${today} |
+   <strong>Time:</strong> ${time}`;
+  
+  let dateSpanMobile = document.getElementById("dateSpanMobile");
+  dateSpanMobile.innerHTML = `<strong>Date:</strong> ${today} |
+   <strong>Time:</strong> ${time}`;
+  
+  //All the lets in the house
+  let taskName = document.getElementById("taskName");
+  let assignedTo = document.getElementById("assignedTo");
+  let form = document.getElementById("form");
+  let setStatus = document.getElementById("setStatus");
+  let errorElement = document.getElementById("errorMsg");
+  let description = document.getElementById("description");
+  var modal = document.getElementById("form");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementById("closebtn");
+  let dueDate = document.getElementById("dueDate");
+  let formDelete = document.getElementById("formDelete");
+  let closebtnedit = document.getElementById("closebtnedit");
+  let card1 = document.getElementsByClassName("card1");
+  
+  let modalOverlay = document.getElementById("modalOverlay");
+  let mobileAddTaskBtn = document.getElementById("addTaskBtnMobile");
+  
+  //Click events
+  
+  for (let i = 0; i < card1.length; i++) {
+      card1[i].addEventListener('click', ()=>{editTask()})
+      }
+      
+      function editTask(){
+          formDelete.style.display = 'block'
+          modalOverlay.style.opacity = "0.3";
+          modalOverlay.style.backgroundColor = "gray";
+      };
+  
+  for (let i = 0; i < card1.length; i++) {
+    card1.item(i).addEventListener("click", function () {
+      formDelete.style.display = "block";
+      modalOverlay.style.opacity = "0.3";
+      modalOverlay.style.backgroundColor = "gray";
+    });
+  }
+  btn.onclick = function () {
+    modal.style.display = "block";
+    modalOverlay.style.opacity = "0.3";
+    modalOverlay.style.backgroundColor = "gray";
+  };
+  span.onclick = function () {
+    modal.style.display = "none";
+    modalOverlay.style.opacity = "1";
+    modalOverlay.style.backgroundColor = "transparent";
+  };
+  closebtnedit.onclick = function () {
     formDelete.style.display = "none";
     modalOverlay.style.opacity = "1";
     modalOverlay.style.backgroundColor = "transparent";
-  }
-mobileAddTaskBtn.onclick = function() {
+  };
+  mobileAddTaskBtn.onclick = function () {
     modal.style.display = "block";
     modalOverlay.style.opacity = "0.3";
-  modalOverlay.style.backgroundColor = "gray";
-   
+    modalOverlay.style.backgroundColor = "gray";
   };
-
- 
-let formValidated = false;
-
-//Validating the form fields
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if(taskName.value === ''){
-        messages.push('Task Name is Required')
+  
+  let formValidated = false;
+  
+  //Validating the form fields
+  form.addEventListener("submit", (e) => {
+    let messages = [];
+    if (taskName.value === "") {
+      messages.push("Task Name is Required");
     }
-
-    if (taskName.value.length < 8){
-        messages.push('Task Name must be longer than 8 characters')
+  
+    if (taskName.value.length < 8) {
+      messages.push("Task Name must be longer than 8 characters");
     }
-    if (assignedTo.value == "" ){
-        messages.push('Task must be assigned')
+    if (assignedTo.value == "") {
+      messages.push("Task must be assigned");
     }
-    if (setStatus.value == ""){
-        messages.push('Please set a status.')
+    if (setStatus.value == "") {
+      messages.push("Please set a status.");
     }
-    if (dueDate.value === ""){
-        messages.push('Please set a due date')
+    if (dueDate.value === "") {
+      messages.push("Please set a due date");
     }
-    if (description.value == ""){
-        messages.push('Please write a description')
+    if (description.value.length < 20) {
+      messages.push("Please write a description of at least 20 characters");
     }
-    if (description.value.length < 20){
-        messages.push('Please write a description of at least 20 characters')
-    }
-    if (messages.length > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join('. ')
+    if (messages.length > 0) {
+      e.preventDefault();
+      errorElement.innerText = messages.join(". ");
     } else {
-        return formValidated = true;
+      console.log("hello");
+      messages = [];
+      errorElement.innerText = messages;
+      return (formValidated = true);
     }
-    
-})
-
-//Grey out past dates- making only future dates clickable
-dueDate.addEventListener("click", function () {
+  });
+  
+  //Grey out past dates- making only future dates clickable
+  dueDate.addEventListener("click", function () {
     let today = new Date();
     let dateToday = String(today.getDate()).padStart(2, "0");
     let monthToday = String(today.getMonth() + 1).padStart(2, "0");
@@ -131,118 +126,166 @@ dueDate.addEventListener("click", function () {
     let minDate = `${yearToday}-${monthToday}-${dateToday}`;
     dueDate.min = minDate;
   });
-
- //Begin Javascript for adding todo
-
- //class constructor
- class NewTask {
-    constructor(newTaskName, newAssignTo, newDueDate, newSelectStatus, newAddDescription, newID) {
+  
+  dueDate2.addEventListener("click", function () {
+    let today = new Date();
+    let dateToday = String(today.getDate()).padStart(2, "0");
+    let monthToday = String(today.getMonth() + 1).padStart(2, "0");
+    let yearToday = today.getFullYear();
+    let minDate = `${yearToday}-${monthToday}-${dateToday}`;
+    dueDate2.min = minDate;
+  });
+  
+  //Begin Javascript for adding todo
+  
+  
+  let latestId = [1];
+  
+  //class constructor
+  class NewTask {
+    constructor(
+      newTaskName,
+      newAssignTo,
+      newDueDate,
+      newSelectStatus,
+      newAddDescription,
+      latestId,
+    ) {
       this.newTaskName = newTaskName;
       this.newAssignTo = newAssignTo;
       this.newDueDate = newDueDate;
       this.newSelectStatus = newSelectStatus;
       this.newAddDescription = newAddDescription;
-      this.newID = Math.floor(Math.random()*10000);
-    }
-    incrementnewID() {
-        this.newID++;
-    }
+      this.latestId = latestId;
+    };
+  
+  
+  
+    
   }
-
-
-//assigning lets
-
-let toDoItems = [];
- let inProgressItems = [];
- let reviewItems = [];
- let doneItems = [];
-let modalBtn = document.getElementById('modalBtn');
-
-let cardsToDo = document.getElementById('cardsToDo');
-let cardsinProgress = document.getElementById('cardsinProgress');
-let cardsReview = document.getElementById('cardsReview');
-let cardsDone = document.getElementById('cardsDone');
-
-
-/// extract information from input fields on submit button click
-
-form.addEventListener('submit',
-    function extractData(){
-
-    let ourNewTask = new NewTask (`${taskName.value}`,`${assignedTo.value}`,`${dueDate.value}`,
-        `${setStatus.value}`,`${description.value}`,`${description.value}`);
-
-    if (setStatus.value === "modalToDo") {
-        toDoItems.push(ourNewTask);
-
-        let card = `<div class=newCard><span><img src="./Resources/redbox.png" alt=""></span>
-            <h3> ${taskName.value} </h3> 
-            <p class="taskDescriptionText"> ${description.value} </p>
-            <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
-            <hr> 
-            <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p></div>`
-
-        function addNewCardDivToDo() {
-            const newDiv = document.createElement("div");
-            cardsToDo.insertAdjacentElement('beforeend', newDiv);
-            newDiv.classList.add("card1");
-            newDiv.innerHTML = card;
-        };
-         addNewCardDivToDo();
-         
-    } if (setStatus.value === "modalInProgress") {
-        inProgressItems.push(ourNewTask);
-
-        let card = `<span><img src="./Resources/yellowbox.png" alt=""></span>
-            <h3> ${taskName.value} </h3> 
-            <p class="taskDescriptionText"> ${description.value} </p>
-            <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
-            <hr> 
-            <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p>`
-
-        function addNewCardDivInProgress() {
-            const newDiv = document.createElement("div");
-            cardsinProgress.insertAdjacentElement('beforeend', newDiv);
-            newDiv.classList.add("card1");
-            newDiv.innerHTML = card;
-        };
-        addNewCardDivInProgress();
-
-    } if (setStatus.value === "modalReview") {
-        reviewItems.push(ourNewTask);
-
-        let card = `<span><img src="./Resources/bluebox.png" alt=""></span>
-            <h3> ${taskName.value} </h3> 
-            <p class="taskDescriptionText"> ${description.value} </p>
-            <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
-            <hr> 
-            <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p>`
-
-        function addNewCardDivCardsReviews() {
-            const newDiv = document.createElement("div");
-            cardsReview.insertAdjacentElement('beforeend', newDiv);
-            newDiv.classList.add("card1");
-            newDiv.innerHTML = card;
-        };
-        addNewCardDivCardsReviews();
-
-    } if (setStatus.value === "modalDone") {
-        doneItems.push(ourNewTask);
-
-        let card = `<span><img src="./Resources/greenbox.png" alt=""></span>
-            <h3> ${taskName.value} </h3> 
-            <p class="taskDescriptionText"> ${description.value} </p>
-            <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
-            <hr> 
-            <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p>`
-
-        function addNewCardDivcardsDone() {
-            const newDiv = document.createElement("div");
-            cardsDone.insertAdjacentElement('beforeend', newDiv);
-            newDiv.classList.add("card1");
-            newDiv.innerHTML = card;
-        };
-        addNewCardDivcardsDone();
+  
+  //assigning lets
+  
+  let toDoItems = [];
+  
+  let inProgressItems = [];
+  let reviewItems = [];
+  let doneItems = [];
+  let modalBtn = document.getElementById("modalBtn");
+  let cardsToDo = document.getElementById("cardsToDo");
+  let cardsinProgress = document.getElementById("cardsinProgress");
+  let cardsReview = document.getElementById("cardsReview");
+  let cardsDone = document.getElementById("cardsDone");
+  
+  /// extract information from input fields on submit button click
+  
+  function resetFormClearModal() {
+      form.reset();
+      modal.style.display = "none";
+      modalOverlay.style.opacity = "1";
+      modalOverlay.style.backgroundColor = "transparent";
+      formValidated = false;
+  
+  }
+  
+  form.addEventListener("submit", function extractData() {
+  
+    let ourNewTask = new NewTask(
+      taskName.value,
+      assignedTo.value,
+      dueDate.value,
+      setStatus.value,
+      description.value,
+      latestId.at(-1),
+    );
+  
+  
+    if (formValidated === true && setStatus.value === "modalToDo") {
+      toDoItems.push(ourNewTask);
+  
+      let card = `<div class=newCard><span><img src="./Resources/redbox.png" alt=""></span>
+              <h3> ${taskName.value} </h3> 
+              <p class="taskDescriptionText"> ${description.value} </p>
+              <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
+              <hr> 
+              <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p></div>`;
+  
+      function addNewCardDivToDo() {
+        const newDiv = document.createElement("div");
+        cardsToDo.insertAdjacentElement("beforeend", newDiv);
+        newDiv.classList.add("card1");
+        newDiv.addEventListener('click', ()=>{editTask(),window.scrollTo(0, 0)})
+        newDiv.innerHTML = card;
+      }
+      addNewCardDivToDo();
+      resetFormClearModal();
     }
-
-})
+    if (formValidated === true && setStatus.value === "modalInProgress") {
+      inProgressItems.push(ourNewTask);
+  
+      let card = `<span><img src="./Resources/yellowbox.png" alt=""></span>
+              <h3> ${taskName.value} </h3> 
+              <p class="taskDescriptionText"> ${description.value} </p>
+              <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
+              <hr> 
+              <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p>`;
+  
+      function addNewCardDivInProgress() {
+        const newDiv = document.createElement("div");
+        cardsinProgress.insertAdjacentElement("beforeend", newDiv);
+        newDiv.classList.add("card1");
+        newDiv.addEventListener('click', ()=>{editTask(),window.scrollTo(0, 0);})
+        newDiv.innerHTML = card;
+      }
+      addNewCardDivInProgress();
+      resetFormClearModal();
+    }
+    if (formValidated === true && setStatus.value === "modalReview") {
+      reviewItems.push(ourNewTask);
+  
+      let card = `<span><img src="./Resources/bluebox.png" alt=""></span>
+              <h3> ${taskName.value} </h3> 
+              <p class="taskDescriptionText"> ${description.value} </p>
+              <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
+              <hr> 
+              <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p>`;
+  
+      function addNewCardDivCardsReviews() {
+        const newDiv = document.createElement("div");
+        cardsReview.insertAdjacentElement("beforeend", newDiv);
+        newDiv.classList.add("card1");
+        newDiv.addEventListener('click', ()=>{editTask(),window.scrollTo(0, 0)})
+        newDiv.innerHTML = card;
+      }
+      addNewCardDivCardsReviews();
+      resetFormClearModal();
+    }
+    if (formValidated === true && setStatus.value === "modalDone") {
+      doneItems.push(ourNewTask);
+  
+      let card = `<span><img src="./Resources/greenbox.png" alt=""></span>
+              <h3> ${taskName.value} </h3> 
+              <p class="taskDescriptionText"> ${description.value} </p>
+              <img class= "profileCard" src="./Resources/ProfileUser1.png"> 
+              <hr> 
+              <p class="dueDateText"><strong>DUE:</strong><span>${dueDate.value}</span></p>`;
+  
+      function addNewCardDivcardsDone() {
+        const newDiv = document.createElement("div");
+        cardsDone.insertAdjacentElement("beforeend", newDiv);
+        newDiv.classList.add("card1");
+        newDiv.addEventListener('click', ()=>{editTask(),window.scrollTo(0, 0)})
+        newDiv.innerHTML = card;
+      }
+      addNewCardDivcardsDone();
+      resetFormClearModal();
+    }
+  function addToArray() {
+  let x = latestId.at(-1);
+  x++;
+  latestId.push(x);
+  }
+  addToArray();
+  }
+  );
+  
